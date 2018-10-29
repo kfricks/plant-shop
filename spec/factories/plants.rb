cc6a1f839b38ec3b20b4c5bf5c48202f8faae234
Katie Fricks <katiefricks@gmail.com>
2018-10-29 15:52:56 -0400

    Added plants model

---
+++ spec/factories/plants.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :plant do
+    symbol { "MyString" }
+    synonym_symbol { "MyString" }
+    scientific_name { "MyString" }
+    common_name { "MyString" }
+    family_common_name { "MyString" }
+    duration { "MyString" }
+    growth_habit { "MyString" }
+    image_gallery { "MyString" }
+    active_growth_period { "MyString" }
+    growth_rate { "MyString" }
+    propogated_by_cuttings { "MyString" }
+  end
+end
