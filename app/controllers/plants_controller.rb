82cde0572caef4e9f3fd3e1d62e851b68dfe5752
Katie Fricks <katiefricks@gmail.com>
2018-10-31 12:44:17 -0400

    updated plants controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -5,15 +5,35 @@ class PlantsController < ApplicationController
   end
 
   def show
+    @plant = Plant.find(params[:id])
   end
 
+#renders form (get new)
   def new
+    @plant = Plant.new
   end
 
+#posts
+  def create
+    # not positive this is right
+    @plant = Plant.find(params[:id])
+    @plant.create(plant_params.merge(user: current_user))
+  end
+
+#renders form (get edit)
   def edit
+    @plant = Plant.find(params[:id])
   end
 
+#posts
   def update
+    @plant = Plant.find(params[:id])
+    if @plant.update(plant_params.merge(user: current_user))
+      flash.now[:success] = "Your plant has been saved."
+      # redirect_to CURRENTUSERSPLANTS
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -22,7 +42,8 @@ class PlantsController < ApplicationController
 private
 
   def plant_params
-    params.require(:plant).permit(:scientific_name, :common_name, :family, :duration, :growth_habit, :growth_period, :growth_rate)
+    # params.require(:plant).permit(:scientific_name, :common_name, :family, :duration, :growth_habit, :growth_period, :growth_rate)
+    params.require(:plant).permit(:plant_type_id, :description)
   end
 
 end
