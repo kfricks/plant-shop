6a5f8f62c0cff06f212a9c16578f22764feb1e77
Katie Fricks <katiefricks@gmail.com>
2018-10-31 13:52:41 -0400

    react component loads on screen

---
+++ app/javascript/components/LiveSearch.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {Typeahead} from 'react-bootstrap-typeahead'; // ES2015
+class LiveSearch extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        "Hello!"
+      </React.Fragment>
+    );
+  }
+}
+
+export default LiveSearch
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'plant_types/index'
   get 'plant_types/show'
-  get 'plants/index'
-  get 'plants/show'
-  get 'plants/edit'
+
+  resources :plants
+
   devise_for :users
   root to: "plants#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
