6068ed26885b84389a482c34478aa700bfc4859e
Katie Fricks <katiefricks@gmail.com>
2018-11-05 17:48:34 -0500

    Removing questionable line

--- app/javascript/components/PlantListItem.js
+++ app/javascript/components/PlantListItem.js
@@ -6,7 +6,7 @@ class PlantListItem extends React.Component {
       <React.Fragment>
         {/* I'm a plant */}
         <div className="plant-list-item-bg">
-        <img src="{this.props.plant.image}"/>
+      {/* <img src="{this.props.plant.image}"/> */}
        {/* #image tag with href that points to --> {this.props.plant.image} */}
         {this.props.plant.common_name} <em>({this.props.plant.scientific_name})</em>
         <br/>
