7ff2242e8c763d939073e11759138c1cab202a95
Katie Fricks <katiefricks@gmail.com>
2018-11-07 15:28:27 -0500

    using react-dnd library up to drag source section

--- app/javascript/components/PlantListItem.js
+++ app/javascript/components/PlantListItem.js
@@ -1,17 +1,19 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
+import { DragSource } from "react-dnd";
+
 class PlantListItem extends React.Component {
-  render () {
+  render() {
     return (
       <div className="plant-list-item-bg">
-        <img src={this.props.plant.image} className="plant-list-item-image"/>
+        <img src={this.props.plant.image} className="plant-list-item-image" />
         <br />
-        {/* #image tag with href that points to --> {this.props.plant.image} */}
-        {this.props.plant.common_name} <em>({this.props.plant.scientific_name})</em>
-        <br/>
+        {this.props.plant.common_name}{" "}
+        <em>({this.props.plant.scientific_name})</em>
+        <br />
       </div>
     );
   }
 }
 
-export default PlantListItem
+export default PlantListItem;
