9675c9ba52303e4e40857380fb6efd098f5ff942
Katie Fricks <katiefricks@gmail.com>
2018-11-08 14:14:50 -0500

    users can now add plants to trade zone, and the plant will be removed from original plant list

--- app/javascript/components/TradeItems.js
+++ app/javascript/components/TradeItems.js
@@ -1,12 +1,27 @@
-import React from "react"
-import PropTypes from "prop-types"
-import PlantListItem from './PlantListItem';
+import React from "react";
+import PropTypes from "prop-types";
+import PlantListItem from "./PlantListItem";
+import { Droppable } from "react-beautiful-dnd";
+
 class TradeItems extends React.Component {
-  render () {
+  render() {
     return (
-      <div>TRADE ITEMS</div>
+      <Droppable droppableId={this.props.id}>
+        {provided => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            <section className="trade-section">
+              {this.props.plants.map((plant, index) => {
+                return (
+                  <PlantListItem plant={plant} key={plant.id} index={index} />
+                );
+              })}
+              {provided.placeholder}
+            </section>
+          </div>
+        )}
+      </Droppable>
     );
   }
 }
 
-export default TradeItems
+export default TradeItems;
