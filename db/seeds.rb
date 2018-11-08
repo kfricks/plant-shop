523ea93b02e6bebd2fe6109d065d45aee663e2ae
Katie Fricks <katiefricks@gmail.com>
2018-11-07 19:50:17 -0500

    Plant_type common name is now titelcase straight from database and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,15 @@ ActiveRecord::Base.transaction do
   end
 
   CSV.foreach(Rails.root.join('lib/data/plant_types.csv'), headers: true) do |row|
-    PlantType.create! row.to_hash
+    PlantType.create!({
+     scientific_name: row['scientific_name'],
+     common_name: row['common_name'].to_s.titleize
+     family: row['family'].to_s.titelize,
+     duration: row['duration'].to_s.titelize,
+     growth_habit: row['growth_habit'].to_s.titelize,
+     growth_period: row['growth_period'].to_s.titelize,
+     growth_rate: row['growth_rate'].to_s.titelize
+     })
     print "📗"
   end
 
