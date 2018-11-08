dd484b0b7f10a88cf0dde9da856c1de1ae3ff49d
Katie Fricks <katiefricks@gmail.com>
2018-11-07 23:05:57 -0500

    formatting

--- app/javascript/components/PlantList.js
+++ app/javascript/components/PlantList.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Droppable } from "react-beautiful-dnd";
 import PlantListItem from "./PlantListItem";
+
 class PlantList extends React.Component {
   render() {
     return (
@@ -9,7 +10,7 @@ class PlantList extends React.Component {
         {provided => (
           <div ref={provided.innerRef} {...provided.droppableProps}>
             {this.props.plants.map((plant, index) => {
-              return <PlantListItem plant={plant} key={plant.id} />;
+              return <PlantListItem plants={plants} key={plant.id} />;
             })}
             {provided.placeholder}
           </div>
