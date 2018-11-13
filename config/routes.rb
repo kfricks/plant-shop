3d9a932d4cbe557b96e7b36a690e9c48825f5618
Katie Fricks <katiefricks@gmail.com>
2018-11-13 15:40:18 -0500

    Trade button now has working conditionals

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :plants
   get "trades/create_by_owner/:user_id/plants/:plant_id", to: "trades#create_by_owner", as: "create_trade_by_owner"
+  get "trades/:trade_id/approve/", to: "trades#approve", as: "approve"
   resources :trades
 
   resources :users, only: [:show]
