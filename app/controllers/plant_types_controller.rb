4beeaad0dad39af765006cde06a2acb030fc8873
Katie Fricks <katiefricks@gmail.com>
2018-11-16 09:08:13 -0500

    white-space

--- app/controllers/plant_types_controller.rb
+++ app/controllers/plant_types_controller.rb
@@ -1,8 +1,6 @@
 class PlantTypesController < ApplicationController
   def index
     @plant_types = PlantType.all
-    # @plant_type = PlantType.find(params[:id])
-    # @common_name = plant_type.common_name
   end
 
   def show
@@ -14,7 +12,6 @@ class PlantTypesController < ApplicationController
     respond_to do |format|
       format.json { render "index" }
     end
-    #render json: response
   end
 
 private
@@ -23,7 +20,6 @@ private
     params.require(:plant_type).permit(:scientific_name, :common_name, :family, :duration, :growth_habit, :growth_period, :growth_rate)
   end
 
-  # trying to set it up for users
   def user_params
     params.require(:user).permit(:first_name, :last_name)
   end
